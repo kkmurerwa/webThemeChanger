a8daa8832895bdbefa9b2d71dc825feb629b450a
xwaxes01 <xwaxes@gmail.com>
2019-07-07 10:07:03 +0300

    Added css for small device widths and scaling button and text

--- js/webTheme.js
+++ js/webTheme.js
@@ -5,13 +5,11 @@ let currentTheme = 0;
 function toggleTheme() {
 
     if (currentTheme == 0) {
-        // alert("Making it dark");
         document.body.style.background = "var(--background-color-dark-mode)";
         document.body.style.color = "var(--main-color-dark-mode)";
         document.getElementById("placeholder-text").innerHTML = "I am dark theme.";
         currentTheme = 1;
     } else {
-        // alert("Making it light");
         document.body.style.background = "var(--background-color)";
         document.body.style.color = "var(--main-color)";
         document.getElementById("placeholder-text").innerHTML = "I am light theme.";
