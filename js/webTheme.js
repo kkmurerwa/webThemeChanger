9f68e1d738c109812e32fb7d953a4b73d7126a88
xwaxes01 <xwaxes@gmail.com>
2019-07-07 10:17:12 +0300

    Changed color of button in different modes

--- js/webTheme.js
+++ js/webTheme.js
@@ -7,12 +7,16 @@ function toggleTheme() {
     if (currentTheme == 0) {
         document.body.style.background = "var(--background-color-dark-mode)";
         document.body.style.color = "var(--main-color-dark-mode)";
+        document.getElementById("toggle-theme").style.background = "gray"
+        document.getElementById("toggle-theme").style.color = "var(--main-color-dark-mode)"
         document.getElementById("placeholder-text").innerHTML = "I am dark theme.";
         currentTheme = 1;
     } else {
         document.body.style.background = "var(--background-color)";
         document.body.style.color = "var(--main-color)";
         document.getElementById("placeholder-text").innerHTML = "I am light theme.";
+        document.getElementById("toggle-theme").style.background = "silver"
+        document.getElementById("toggle-theme").style.color = "var(--main-color)"
         currentTheme = 0;
     }
 }
